bb27be7e869bd6b632e6e8711fb0e25850c5253f
Ruteski <lruteski@gmail.com>
2023-09-01 15:54:40 -0300

    alguns fundamentos

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,31 @@
 // import http from 'node:http' // "type": "module", -> no packge.json para poder usar o import/export
 import http from 'node:http';
 
-const server = http.createServer((req, res) => res.end('Hello, lincoln!'))
+const users = []
+
+const server = http.createServer((req, res) => {
+  const {method, url} = req;
+
+  console.log(method, url)
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Lincoln Ruteski',
+      email: 'email@remail.com'
+    })
+
+    return res.writeHead(201).end()
+
+  }
+
+  return res.writeHead(404).end('')
+})
 
 server.listen(3333);
