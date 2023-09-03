415085694fb7b880edf1a69b6d2289a70bc7586f
Ruteski <lruteski@gmail.com>
2023-09-03 20:43:20 -0300

    Rotas da aplicação - Criando regex dos parâmetros

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -5,7 +5,7 @@ import fs from 'node:fs/promises'
 
 const databasePath = new URL('../db.json', import.meta.url)
 
-console.log(databasePath)
+// console.log(databasePath)
 
 export class Database {
   #database = {};
