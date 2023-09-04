5d3fa6e3fcc29409b2a2a2cb501c926ff212c96a
Ruteski <lruteski@gmail.com>
2023-09-03 22:52:11 -0300

    Estrutura da aplicação - Configurando ESLint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
 app.get('/hello', () => {
-  return 'hello world'
-})
+  return 'hello world';
+});
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server running in port 3333!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running in port 3333!');
+  });
