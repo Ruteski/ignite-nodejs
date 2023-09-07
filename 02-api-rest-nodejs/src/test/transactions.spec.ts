bb86139e072e1c9921d69b77bd5c690c2ae04803
Ruteski <lruteski@gmail.com>
2023-09-07 19:18:37 -0300

    Testes automatizados - Testando listagem de transações

--- 02-api-rest-nodejs/src/test/transactions.spec.ts
+++ 02-api-rest-nodejs/src/test/transactions.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test, beforeAll, afterAll, describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from '../app';
+import { create } from 'domain';
+import exp from 'constants';
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -28,5 +30,30 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server).post('/transactions').send({
+      title: 'new transaction',
+      amount: 5000,
+      type: 'credit',
+    });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200);
+
+    // expect(listTransactionsResponse.body).toEqual([
+    //   {
+    //     id: expect.any(String),
+    //   },
+    // ]);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 5000,
+      }),
+    ]);
   });
 });
