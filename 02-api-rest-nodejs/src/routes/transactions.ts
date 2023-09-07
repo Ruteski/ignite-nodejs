677f5ee645679ab8c3f172d4fd2f3b61ce3c133d
Ruteski <lruteski@gmail.com>
2023-09-07 16:45:18 -0300

    Implementando rotas - Listagem de transações

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -4,6 +4,31 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamSchema.parse(req.params);
+
+    // const transaction = await knex('transactions')
+    //   .select()
+    //   .where({
+    //     id,
+    //   })
+    //   .first();
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
   app.post('/', async (req, rep) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
