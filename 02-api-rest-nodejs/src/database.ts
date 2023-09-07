3f41c98503d0f360e1037b33ce15df4bf5491efe
Ruteski <lruteski@gmail.com>
2023-09-07 13:28:22 -0300

    Banco de dados - Tratando env com Zod

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -2,24 +2,20 @@
 // yarn knex -- -h
 // yarn knex -- migrate:make
 
-import 'dotenv/config';
 import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
 
 console.log(process.env);
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found.');
-}
 
 export const configKnex: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: process.env.DATABASE_MIGRATIONS,
+    directory: env.DATABASE_MIGRATIONS,
   },
 };
 
---
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  DATABASE_MIGRATIONS: z.string(),
+  PORT: z.string().default('3333'),
+});
+
+// export const env = envSchema.parse(process.env);
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format());
+
+  throw new Error('Invalid environment variables');
+}
+
+export const env = _env.data;
