1b6cd351dbfdafa04f56aa8bf68b223a8d19a04d
Ruteski <lruteski@gmail.com>
2023-09-07 19:05:17 -0300

    Testes automatizados - Categorizando os testes

---
+++ 02-api-rest-nodejs/src/test/transactions.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test, beforeAll, afterAll, describe, it } from 'vitest';
+import request from 'supertest';
+import { app } from '../app';
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a new transaction', async () => {
+    // const response = await request(app.server).post('/transactions').send({
+    //   title: 'new transaction',
+    //   amount: 5000,
+    //   type: 'credit',
+    // });
+    //
+    // expect(response.statusCode).toEqual(201);
+
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201);
+  });
+});
