7439e12def4827a8c69a7b747d0a2ceb814fc895
Ruteski <lruteski@gmail.com>
2023-09-07 19:00:21 -0300

    Testes automatizados - Testando criação de transações

---
+++ 02-api-rest-nodejs/src/app.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify';
+import cookie from '@fastify/cookie';
+import { env } from './env';
+import { transactionRoutes } from './routes/transactions';
+
+export const app = fastify();
+
+app.register(cookie);
+
+// executa em qualquer rota chamada aqui dentro
+app.addHook('preHandler', async (req, rep) => {
+  console.log(`Executando de modo global para todas as rotas da aplicacao [${req.method}] ${req.url}`);
+});
+
+// o fastify roda os plugin em ordem que foram registrados, cuidar com isso
+app.register(transactionRoutes, {
+  prefix: 'transactions',
+});
+
+app.get('/hello', async () => {
+  return 'Hello World';
+});
--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,25 +1,5 @@
-import fastify from 'fastify';
-import cookie from '@fastify/cookie';
+import { app } from './app';
 import { env } from './env';
-import { transactionRoutes } from './routes/transactions';
-
-const app = fastify();
-
-app.register(cookie);
-
-// executa em qualquer rota chamada aqui dentro
-app.addHook('preHandler', async (req, rep) => {
-  console.log(`Executando de modo global para todas as rotas da aplicacao [${req.method}] ${req.url}`);
-});
-
-// o fastify roda os plugin em ordem que foram registrados, cuidar com isso
-app.register(transactionRoutes, {
-  prefix: 'transactions',
-});
-
-app.get('/hello', async () => {
-  return 'Hello World';
-});
 
 app
   .listen({
