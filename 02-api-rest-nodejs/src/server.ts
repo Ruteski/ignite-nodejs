79e0fc29d67599f2aa7bb5c9939617da9af21e68
Ruteski <lruteski@gmail.com>
2023-09-07 21:44:21 -0300

    configuracao para deploy

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app';
 import { env } from './env';
 
+const host = 'RENDER' in process.env ? 'o.o.o.o' : 'localhost';
+
 app
   .listen({
-    port: Number(env.PORT),
+    port: env.PORT,
+    host,
   })
   .then(() => {
     console.log('HTTP Server running in port 3333!');
