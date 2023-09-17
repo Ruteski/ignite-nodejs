6125a8fa8ceaf1023a50df4ecefc70f4aa7a107a
Ruteski <lruteski@gmail.com>
2023-09-17 09:49:26 -0300

    Controllers & Testes E2E - Controller de perfil e middleware de autenticação

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -2,6 +2,7 @@ import { userRegister } from '@/http/controllers/register';
 import { FastifyInstance } from 'fastify';
 import { authenticate } from '@/http/controllers/authenticate';
 import { profile } from '@/http/controllers/profile';
+import { verifyJwt } from '@/http/middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', userRegister);
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate);
 
   /* Routes Authenticated */
-  app.get('/me', profile);
+  app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
