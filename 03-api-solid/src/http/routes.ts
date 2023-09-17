83c6f84a4e7cb2c7b6d2b312dae3d7b8be1e8125
Ruteski <lruteski@gmail.com>
2023-09-17 09:05:40 -0300

    Controllers & Testes E2E - Princípios da autenticação JWT

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -1,8 +1,13 @@
 import { userRegister } from '@/http/controllers/register';
 import { FastifyInstance } from 'fastify';
 import { authenticate } from '@/http/controllers/authenticate';
+import { profile } from '@/http/controllers/profile';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', userRegister);
+
   app.post('/sessions', authenticate);
+
+  /* Routes Authenticated */
+  app.get('/me', profile);
 }
