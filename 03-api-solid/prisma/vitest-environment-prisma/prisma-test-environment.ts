47bffe8774ce53fe8e925123da979596b732f956
Ruteski <lruteski@gmail.com>
2023-09-18 15:10:26 -0300

    Controllers & Testes E2E - Test Environment do Prisma

--- 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,43 @@
+import 'dotenv/config';
+
 import { Environment } from 'vitest';
+import { randomUUID } from 'node:crypto';
+import { PrismaClient } from '@prisma/client';
+import { execSync } from 'node:child_process';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('setup');
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    console.log('url', databaseURL);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy');
 
     return {
       async teardown() {
-        console.log('teardown');
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
