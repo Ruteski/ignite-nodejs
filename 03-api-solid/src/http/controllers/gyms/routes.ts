1a7b07c535e02ca418d9e2a64707246d8a9c7298
Ruteski <lruteski@gmail.com>
2023-09-18 16:00:28 -0300

    Controllers & Testes E2E - Outros controllers da academia

--- 03-api-solid/src/http/controllers/gyms/routes.ts
+++ 03-api-solid/src/http/controllers/gyms/routes.ts
@@ -1,7 +1,16 @@
 import { FastifyInstance } from 'fastify';
+
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
+import { search } from './search';
+import { nearby } from './nearby';
+import { create } from './create';
 
 export async function gymsRoutes(app: FastifyInstance) {
   // todas as rotas dentro desse arquivo vao chamar o middleware para verificar se esta autenticado
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
+  app.get('/gyms/nearby', nearby);
+
+  app.post('/gyms', create);
 }
