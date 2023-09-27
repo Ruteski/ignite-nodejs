4233f4eb162ee87fc9bf6a37a6244c1be05be01b
Ruteski <lruteski@gmail.com>
2023-09-27 13:17:02 -0300

    Controller: Deletar Resposta

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -19,6 +19,8 @@ import { AnswerQuestionController } from '@/infra/http/controllers/answer-questi
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question';
 import { EditAnswerController } from '@/infra/http/controllers/edit-answer.controller';
 import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer';
+import { DeleteAnswerController } from '@/infra/http/controllers/delete-answer.controller';
+import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -32,6 +34,7 @@ import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-ans
     DeleteQuestionController,
     AnswerQuestionController,
     EditAnswerController,
+    DeleteAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -43,6 +46,7 @@ import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-ans
     DeleteQuestionUseCase,
     AnswerQuestionUseCase,
     EditAnswerUseCase,
+    DeleteAnswerUseCase,
   ],
 })
 export class HttpModule {}
